c16de68bea77538f0e2b1722713c0fdda35e1e7a
jkwinta <jkwinta@gmail.com>
2020-02-03 21:23:19 -0500

    List zipped book files in different orders

---
+++ gutenberg_files/files.py
@@ -0,0 +1,45 @@
+import dotenv
+import os
+import pathlib
+import random
+
+dotenv.load_dotenv()
+
+
+class GutenbergFiles:
+
+    def __init__(self, directory=None):
+        if directory is None:
+            directory = os.environ.get('GUTENBERG_DIR')
+        self.directory = directory
+        self.files = None
+
+    def _get_files(self):
+        if self.files is None:
+            if self.directory is None:
+                self.files = []
+            else:
+                dir_path = pathlib.Path(self.directory).absolute()
+                self.files = list(dir_path.rglob('*.zip'))
+                # TODO: Is this appropriate:
+                self.files = [book_path for book_path in self.files
+                              if '-' not in book_path.name]
+        return self.files
+
+    def get_files(self, order='glob'):
+        order = order.strip().lower()
+        if order == 'lexicographical':
+            book_paths = sorted(self._get_files(), key=lambda bp: bp.name)
+        elif order == 'numerical':
+            book_paths = sorted(self._get_files(),
+                                key=lambda bp: int(bp.name.rstrip('.zip')))
+        elif order == 'random':
+            book_paths = random.sample(self._get_files(),
+                                       k=len(self._get_files()))
+        else:
+            book_paths = self._get_files()
+        yield from book_paths
+
+# numbers = [int(b.name.rstrip('.zip').split('-')[0]) for b in books]
+# un_dash_numbers = [int(b.name.rstrip('.zip')) for b in books
+#                    if '-' not in b.name]
